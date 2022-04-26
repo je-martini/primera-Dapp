0eb4c6e548c788763a85537dc840134cfe616f70
je-martini <jesusmartini2017@gmail.com>
2022-04-26 11:26:41 -0500

    se agrego el la carpeta del layout, se agregaron las libreria de web3 injectec y web3 provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from '@web3-react/core';
+import { getLibrary } from './config/web3'; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -10,7 +12,9 @@ root.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
+        <Web3ReactProvider getLibrary={getLibrary}>
         <App />
+        </Web3ReactProvider>
       </ChakraProvider>
     </HashRouter>
   </React.StrictMode>
