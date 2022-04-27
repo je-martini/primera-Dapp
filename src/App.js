981677f505018dfa8a76880069b7339fd892dc0e
je-martini <jesusmartini2017@gmail.com>
2022-04-27 09:07:54 -0500

    se esta creando el documento wallet-data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import { useEffect } from "react"; 
+ 
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/home/index";
-import Web3 from "web3/dist/web3.min";
-
+import MainLayout from "./layout/main";
 
   function App() {
-    useEffect(() => 
-    {
-      if (window.ethereum) {
-          // window.ethereum
-          //   .request({
-          //     method: "eth_requestAccounts",
-          //   })
-          //   .then((accounts) => console.log(accounts));
-    
-          const web3 = new Web3(window.ethereum);
-          web3.eth.requestAccounts().then(console.log);
-      };
-    }, []);
-
   return (
+    <MainLayout>
       <Routes>
         <Route path="/" exact element = { <Home /> } />
       </Routes>
+    </MainLayout>
   );
+  
   }
   
   export default App;
--- src/layout/main/wallet-data/index.js
+++ src/layout/main/wallet-data/index.js
@@ -14,37 +14,12 @@ import {
   import useTruncatedAddress from "../../../hooks/useTruncatedAddress";
   
   const WalletData = () => {
-    const [balance, setBalance] = useState(0);
-    const { active, activate, deactivate, account, error, library } =
-      useWeb3React();
-  
-    const isUnsupportedChain = error instanceof UnsupportedChainIdError;
-  
-    const connect = useCallback(() => {
-      activate(connector);
-      localStorage.setItem("previouslyConnected", "true");
-    }, [activate]);
-  
-    const disconnect = () => {
-      deactivate();
-      localStorage.removeItem("previouslyConnected");
-    };
-  
-    const getBalance = useCallback(async () => {
-      const toSet = await library.eth.getBalance(account);
-      setBalance((toSet / 1e18).toFixed(2));
-    }, [library?.eth, account]);
-  
-    useEffect(() => {
-      if (active) getBalance();
-    }, [active, getBalance]);
-  
-    useEffect(() => {
-      if (localStorage.getItem("previouslyConnected") === "true") connect();
-    }, [connect]);
-  
-    const truncatedAddress = useTruncatedAddress(account);
-  
+    const {activate, account, library } = useWeb3React();
+
+    const connect = () => {
+        activate(connector)
+    }
+
     return (
       <Flex alignItems={"center"}>
         {active ? (
@@ -81,4 +56,4 @@ import {
     );
   };
   
-  export default WalletData;
+  export default WalletData;    
