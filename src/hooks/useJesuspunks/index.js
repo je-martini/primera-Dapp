bb0935abea66fce96632811b84440d7ca6790fbe
je-martini <jesusmartini2017@gmail.com>
2022-05-07 09:11:29 -0500

    se agrego las cosas para el abi

--- src/hooks/useJesuspunks/index.js
+++ src/hooks/useJesuspunks/index.js
@@ -1,5 +1,8 @@
 import { useMemo } from "react";
 import { useWeb3React } from "@web3-react/core";
+import jesuspunksArtifacts from "../../config/web3/artifacts/jesusPunks";
+
+const { address, abi } = jesuspunksArtifacts;
 
 const useJesuspunks = () => {
 
