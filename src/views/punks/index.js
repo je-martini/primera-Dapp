fbe10caebe4467bc6cbe7e83151313840eb126df
je-martini <jesusmartini2017@gmail.com>
2022-05-16 08:59:24 -0500

    se agregaron los detalles finales para la galerria del protyecto

--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -3,30 +3,29 @@ import { Grid } from '@chakra-ui/react';
 import PunkCard from '../../components/punk-card';
 import Loading from '../../components/loading';
 import RequestAccess from '../../components/request-access';
-import { useJesuspunksdata } from '../../hooks/useJesusPunksData';
+import { useJesusPunksdata } from '../../hooks/useJesuspunksData';
 
-const punks = () => {
-    const {active } = useWeb3React();
+const Punks = () => {
+    const { active } = useWeb3React();
 
-    const { punks, loading } = useJesuspunksdata();
+    const { Punks, loading } = useJesusPunksdata();
 
     if (!active) return <RequestAccess />;
     
     return (
         <>
-        {
-            loading ? (
-                <loading /> 
+        {loading ? (
+                <Loading /> 
             ) : (
                 <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
-                {punks.map(({ name, image, tokenId }) => (
+                {Punks.map(({ name, image, tokenId }) => (
                     <PunkCard key={tokenId} image={image} name={name} />
                 ))}
                 </Grid>
                 
             )}
         </> 
-    )
+    );
 };
 
-export default punks;
+export default Punks;
