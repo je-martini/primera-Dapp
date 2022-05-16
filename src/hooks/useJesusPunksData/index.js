7c9cb55b46989a69880e7f5f6dd12b2616cf7b68
je-martini <jesusmartini2017@gmail.com>
2022-05-16 08:21:21 -0500

    se agregaron varias cosas

--- src/hooks/useJesusPunksData/index.js
+++ src/hooks/useJesusPunksData/index.js
@@ -1,12 +1,41 @@
+import { useCallback, useEffect, useState } from "react";
+import useJesuspunks from "../useJesuspunks";
+
 const getPunkData = async ({jesuspunks, tokenId}) => {
     const [
-        tokenURI, dna, owner, AccessoriesType
+        tokenURI,
+        dna, 
+        owner, 
+        AccessoriesType,
+        clotheColor,
+        clotheType,
+        eyeType,
+        eyeBrowType,
+        facialHairColor,
+        facialHairType,
+        hairColor,
+        hatColor,
+        graphicType,
+        mouthType,
+        skinColor,
+        topType,
     ] = await Promise.all([
         jesuspunks.methods.tokenURI(tokenId).call(),
         jesuspunks.methods.tokenDNA(tokenId).call(),
         jesuspunks.methods.ownerOf(tokenId).call(),
         jesuspunks.methods.getAccessoriesType(tokenId).call(),
-        
+        jesuspunks.methods.getClotheColor(tokenId).call(),
+        jesuspunks.methods.getClotheType(tokenId).call(),
+        jesuspunks.methods.getEyeType(tokenId).call(),
+        jesuspunks.methods.getEyeBrowType(tokenId).call(),
+        jesuspunks.methods.getFacialHairColor(tokenId).call(),
+        jesuspunks.methods.getFacialHairType(tokenId).call(),
+        jesuspunks.methods.getHairColor(tokenId).call(),
+        jesuspunks.methods.getHatColor(tokenId).call(),
+        jesuspunks.methods.getGraphicType(tokenId).call(),
+        jesuspunks.methods.getMouthType(tokenId).call(),
+        jesuspunks.methods.getSkinColor(tokenId).call(),
+        jesuspunks.methods.getTopType(tokenId).call(),
     ]);
     const responseMetadata = await fetch(tokenURI);
     const metadata = await responseMetadata.json();
@@ -14,7 +43,19 @@ const getPunkData = async ({jesuspunks, tokenId}) => {
     return {
         tokenId,
         attributes:{
-            AccessoriesType
+            AccessoriesType,
+            clotheColor,
+            clotheType,
+            eyeType,
+            eyeBrowType,
+            facialHairColor,
+            facialHairType,
+            hairColor,
+            hatColor,
+            graphicType,
+            mouthType,
+            skinColor,
+            topType,
         },
         tokenURI,
         dna,
@@ -24,11 +65,29 @@ const getPunkData = async ({jesuspunks, tokenId}) => {
 };
 // plural
 
-const usePlatziPunksData = () => {
+const usejesuspunData = () => {
+    const [punks, setPunks] = useState([]);
+    const [loading,setLoading] = useState([true]);
+    const jesuspunks = useJesuspunks();
 
+    const update = useCallback(async () => {
+        if(jesuspunks){
+            setLoading(true);
+
+            let tokenIds;
+
+            const totalSupply = await jesuspunks.methods.totalSupply().call();
+            tokenIds = new Array(totalSupply).fill().map((_, index) => index); 
+            setLoading(false)
+        }
+    });
+
+    useEffect (() => {
+        update();
+    }, [update]);
 };
 
 // siguiente sera singular
-// const usePlatziPunksData = () => {
+// const usejesuspunData = () => {
 
 //}
